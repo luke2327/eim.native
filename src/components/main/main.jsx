6ee618ec562c87103c9c2e918775994631dd2a10
luke2327 <qwepoi8526@gmail.com>
2019-08-07 00:36:52 +0900

    모듈 설치

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 
 export default function MainHome() {
   return (
     <View style={styles.container}>
       <Text>MainHome</Text>
+      <Button title="go to header page" onPress={Actions.Header}></Button>
     </View>
   );
 }
